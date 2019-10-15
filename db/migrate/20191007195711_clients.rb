31e8cff4c8c398e7db634b6f43ea8de0c90c4c92
Valeria Lopez <valerialopez4991@gmail.com>
2019-10-15 11:31:56 -0500

    clients

--- db/migrate/20191007195711_clients.rb
+++ db/migrate/20191007195711_clients.rb
@@ -1,11 +1,11 @@
-class Users < ActiveRecord::Migration[6.0]
-  def change
-    create_table :clients do |t|
-      t.string  :name
-      t.string :location
-      t.integer :user_id
-
-      t.timestamps
-    end
-  end
-end
+# class Users < ActiveRecord::Migration[6.0]
+#   def change
+#     create_table :clients do |t|
+#       t.string  :name
+#       t.string :location
+#       t.integer :user_id
+#
+#       t.timestamps
+#     end
+#   end
+# end
